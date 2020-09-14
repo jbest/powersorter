2f3fbf0f011eedf8871c6151d7bb8ffbf38617f8
Jason Best <jbest@brit.org>
2020-09-14 14:45:54 -0500

    changed property name

--- config_gen.py
+++ config_gen.py
@@ -11,28 +11,29 @@ files['number_pad'] = 7
 files['base_output_path'] = '/test/out/'
 files['input_path'] = '/test/in/'
 
-# patterns
-patterns = config['patterns'] = {}
+# file_types
+file_types = config['file_types'] = {}
 # TODO create a pattern class to generate each patterns
 # TODO use regex to test compile the pattern, maybe to test?
-web_jpg = patterns['web_jpg'] = {}
+web_jpg = file_types['web_jpg'] = {}
 web_jpg['regex'] = '(?<prefix>BRIT)(?<numerical>\d+)(\.)(?i)(?<ext>jpg|jpeg)'
 web_jpg['destination_path'] = 'web/'
 
-web_derivs = patterns['web_derivs'] = {}
+web_derivs = file_types['web_derivs'] = {}
 web_derivs['regex'] = '(?<prefix>BRIT)(?<numerical>\d+)(?<delimiter>_)(?<size>med|thumb)(\.)(?<ext>.+)'
 web_derivs['destination_path'] = 'web/'
 
-archive_dng = patterns['archive_dng'] = {}
+archive_dng = file_types['archive_dng'] = {}
 archive_dng['regex'] = '(?<prefix>BRIT)(?<numerical>\d+)(\.)(?i)(?<ext>DNG)'
 archive_dng['destination_path'] = 'archive/'
 
 #print(config)
 print(json.dumps(config))
-
+"""
 for pattern in config['patterns']:
     print(pattern, config['patterns'][pattern])
 
+"""
 """
 Sample file strings
 BRIT1000.jpg
