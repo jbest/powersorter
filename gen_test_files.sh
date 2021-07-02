5bbc48b287346a0c05249f9e4ec93e1568118a1e
Jason Best <jbest@brit.org>
2021-07-02 13:03:15 -0500

    cleanup and reorganize for v3

---
+++ gen_test_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# generate test files for sorting
+touch ./input_dir/BRIT123.jpg
+touch ./input_dir/BRIT123_med.jpg
+touch ./input_dir/BRIT123_thumb.jpg
+touch ./input_dir/BRIT123.dng
