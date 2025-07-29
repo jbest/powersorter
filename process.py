07eb158105ffc1852a4eda26d57af33eb3721687
Jason Best <jbest@brit.org>
2025-07-29 17:08:13 -0500

    extensive fixes

--- process.py
+++ process.py
@@ -2,11 +2,10 @@ import csv
 import argparse
 import datetime
 import sys
+import os
 
 import powersorter as ps
-import image_resizer
-
-
+import image_resizer as derivs
 
 CONFIG_FORMAT_REQUIRED = '3.0'
 
@@ -27,33 +26,14 @@ def arg_setup():
     return args
 
 # gather args and settings
-
-
-# create config
-
-
-# generate derivs
-
-# sort
-
-
-# generate urls
-
-if __name__ == '__main__':
-    # initialize settings
-    # set up argparse
+def initialize_settings():
     args = arg_setup()
-        #print(args)
+    #print(args)
     config_file = args['config']
     dry_run = args['dry_run']
     verbose = args['verbose']
     force_overwrite = args['force']
     input_path_override = args['input_path']
-
-    # create settings based on args and config file
-    #settings = ps.Settings(dry_run=dry_run, verbose=verbose, force_overwrite=force_overwrite_confirmed)
-    #Load settings from config
-    #settings.load_config(config_file=config_file)
 
     # use input_path arg to override input_path in config file
     if input_path_override:
@@ -82,8 +62,61 @@ if __name__ == '__main__':
     if not str(settings.config_format) == CONFIG_FORMAT_REQUIRED:
         print('Wrong config format version:', settings.config_format, 'Required:', CONFIG_FORMAT_REQUIRED)
         sys.exit()
+    return(settings)
+
+
+
+if __name__ == '__main__':
+    # initialize settings from args and config file
+    settings = initialize_settings()
 
     #show settings for testing
     attrs = vars(settings)
-    print('Args:')
+    print('Settings:')
     print('\n'.join("%s: %s" % item for item in attrs.items()))
+
+    # generate derivs
+    #TODO add arg to generate derivs
+    # use input path
+    print('Will generate derivs for files at:')
+    print(settings.input_path)
+    # process input_path
+    #TODO provide extensions to process (currently hard coded in image_resizer.py)
+    #TODO pass values for rezie based on cofig params
+    #TODO make image_resizer silent/not verbose
+    #TODO make image_resizer process files that match regex, not whole directory
+
+    for file_type, value in settings.file_types.items():
+        #print('file_type', file_type, 'value', value)
+        #regex = value.get('regex', None)
+        file_regex = value.get('file_regex', None)
+        # Adding regex i flag here to make case-insensitive rather than in config files
+        regex = '(?i)' + settings.catalog_number_regex + file_regex
+        output_sub_path = value.get('output_sub_path', None)
+        output_path = settings.output_base_path.joinpath(output_sub_path)
+        # Check ability to write to directory
+        if not os.access(output_path, os.W_OK | os.X_OK):
+            #TODO log fail
+            print(f'Unable to write to directory: {output_path}')
+        else:
+            # TEMP
+            print(f'Will scan files, input_path:{settings.input_path}, pattern:{regex}, file_type:{file_type}')
+            file_matches = ps.scan_files(path=settings.input_path, pattern=regex, file_type=file_type)
+            """
+            sort_result = sort_files(files=file_matches, \
+                number_pad=number_pad, \
+                folder_increment=folder_increment, \
+                collection_prefix=collection_prefix, \
+                output_path=output_path)
+            sorted_file_count += sort_result.get('sorted_file_count', 0)
+            unmoved_file_count += sort_result.get('unmoved_file_count', 0)
+            """
+            print(file_matches)
+
+    #file_matches = ps.scan_files(path=settings.input_path, pattern=settings.regex, file_type=settings.file_types)
+    #derivs.process_folder(settings.input_path)
+    #print(file_matches)
+
+    # sort
+
+    # generate urls
